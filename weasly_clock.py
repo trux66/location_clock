b4d8a3b0570dc19e73dfabc8f885381cd5f99e36
Bruce Clawson <bruce@clawsonfamily.net>
2017-01-28 11:35:58 -0500

    clean up _type handling routine in on_message callback.

--- weasly_clock.py
+++ weasly_clock.py
@@ -4,24 +4,24 @@ import datetime
 
 # provide switch/case type construct for python
 class switch(object):
-    def __init__(self, value):
-        self.value = value
-        self.fall = False
+  def __init__(self, value):
+    self.value = value
+    self.fall = False
 
-    def __iter__(self):
-        """Return the match method once, then stop"""
-        yield self.match
-        raise StopIteration
-    
-    def match(self, *args):
-        """Indicate whether or not to enter a case suite"""
-        if self.fall or not args:
-            return True
-        elif self.value in args: # changed for v1.5, see below
-            self.fall = True
-            return True
-        else:
-            return False
+  def __iter__(self):
+    """Return the match method once, then stop"""
+    yield self.match
+    raise StopIteration
+  
+  def match(self, *args):
+    """Indicate whether or not to enter a case suite"""
+    if self.fall or not args:
+      return True
+    elif self.value in args: # changed for v1.5, see below
+      self.fall = True
+      return True
+    else:
+      return False
 
 
 # The callback for when the client receives a CONNACK response from the server.
@@ -36,17 +36,19 @@ def on_connect(client, userdata, flags, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
   topic = msg.topic
+  data = json.loads(str(msg.payload))
+  if data['_type']!='card':  # card _type doesn't contain timestamp
+    dt = datetime.datetime.fromtimestamp(data['tst']).strftime('%d-%b-%Y %I:%M:%S %p %Z')
+
   try:
-    data = json.loads(str(msg.payload))
-    dt = datetime.datetime.fromtimestamp(data['tst']).strftime(
-      '%d-%b-%Y %I:%M:%S %p %Z')
+    # print "type is {0}".format(data['_type'])
     for case in switch(data['_type']):
+      if case('lwt'):
+        print "Topic {0} is in mortal peril!".format(topic)
+        break
       if case('location'):
         print "TID = {0} was at {1}, {2} on {3}".format(
           data['tid'], data['lat'], data['lon'], dt)
-        break
-      if case('lwt'):
-        print "TID = {0} is in mortal peril! at {1}".format(data['tid'], dt)
         break
       if case('waypoint'):
         print "TID = {0} reached waypoint {1} on {2}".format(data['tid'], data['desc'], dt)
@@ -61,12 +63,13 @@ def on_message(client, userdata, msg):
             break
           if case(): # default
             print "Not sure what happened in this transition"
-        break
+            break
       if case('beacon', 'cmd', 'steps', 'configuration', 'card', 'waypoints', 'encrypted'):
-        print "payload is valid but I don't care about it"
+        print "Payload is of _type {0}, but I don't care about that type.".format(data['_type'])
         break
       if case(): # default
         print "I don't know what the payload is!"
+
   except:
     print "Cannot decode data on topic {0}".format(topic)
     print("Received message '" + str(msg.payload) + "' on topic '" 
